a6586b31e1f23ab42b7378c4f08d6c153270a357
Fhan Jacson <Jacson.Fhan@adv-fuxionex.com>
2019-09-03 14:48:41 +0800

    Add: Alyle-UI and carousel with gesture component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarouselWithGesturesComponent } from './carousel-with-gestures/carousel-with-gestures.component';
 
+const routes: Routes = [
+        { path: 'carousel', component: CarouselWithGesturesComponent },
+        { path: '', redirectTo: 'carousel', pathMatch: 'full'}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LyThemeModule, LY_THEME } from '@alyle/ui';
+import { MinimaLight } from '@alyle/ui/themes/minima';
+import { LyButtonModule } from '@alyle/ui/button';
+import { LyCarouselModule } from '@alyle/ui/carousel';
+
+import { CarouselWithGesturesComponent } from './carousel-with-gestures/carousel-with-gestures.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselWithGesturesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LyThemeModule.setTheme('minima-light'),
+    LyButtonModule,
+    LyCarouselModule
+
   ],
-  providers: [],
+  providers: [{ provide: LY_THEME, useClass: MinimaLight, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
